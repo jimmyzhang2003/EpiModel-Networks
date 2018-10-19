7bd810c3ff567d75241d59a9f357cb8b9654e1fb
jimmyzhang2003 <42526010+jimmyzhang2003@users.noreply.github.com>
2018-10-18 22:48:19 -0400

    Update network_setup_code.r

--- network_setup_code.r
+++ network_setup_code.r
@@ -1,25 +1,29 @@
-require(knitr)
-require(EpiModel)
-opts_chunk$set(comment = NA, message = FALSE, tidy = FALSE)
-#initializing network
-num.m1 <- 500
-num.m2 <- 500
-nw <- network.initialize(num.m1 + num.m2, bipartite = num.m1, directed = FALSE)
-deg.dist.m1 <- c(0.40, 0.55, 0.04, 0.01)
-deg.dist.m2 <- c(0.48, 0.41, 0.08, 0.03)
-pois.dists <- c(dpois(0:2, lambda = 0.66), ppois(2, lambda = 0.66, lower = FALSE))
-formation <- ~edges + b1degree(0:1) + b2degree(0:1)
-target.stats <- c(330, 200, 275, 240, 205)
-coef.diss <- dissolution_coefs(dissolution = ~offset(edges), duration = 25, d.rate = 0.005)
-est <- netest(nw, formation, target.stats, coef.diss)
-dx <- netdx(est, nsims = 3, nsteps = 500)
+library(EpiModel)
+library(mgm)
+#networksetup
+nw <- network.initialize(n = 100, directed = FALSE)
+nw <- set.vertex.attribute(nw, "race", rbinom(100, 1, 0.5))
+nw <- set.vertex.attribute(nw, "age", sample(18:50, 100, TRUE))
+nw <- set.vertex.attribute(nw, "sex", rep(0:1, each = 50))
+formation <- ~edges + nodefactor("race") + nodematch("race") + nodefactor("sex") + nodematch("sex") + absdiff("age") + concurrent
+target.stats <- c(25, 37.5, 22.5, 33, 5, 100, 10)
+coef.diss <- dissolution_coefs(dissolution = ~offset(edges), duration = 25)
+est <- netest(nw, formation, target.stats, coef.diss, edapprox = TRUE)
+dx <- netdx(est, nsims = 1, nsteps = 100,
+nwstats.formula = ~edges + nodefactor("race", base = 0) +
+nodematch("race") + nodefactor("sex", base = 0) + nodematch("sex") + absdiff("age") + concurrent)
 #parameters
-param <- param.net(inf.prob = 0.3, inf.prob.m2 = 0.1, b.rate = 0.005, b.rate.m2 = NA, ds.rate = 0.005, ds.rate.m2 = 0.005, 
- di.rate = 0.005, di.rate.m2 = 0.005)
-init <- init.net(i.num = 50, i.num.m2 = 50)
-control <- control.net(type = "SI", nsims = 3, nsteps = 500, nwstats.formula = ~edges + meandeg, delete.nodes = TRUE)
-#simulations
+param <- param.net(inf.prob = 0.2, act.rate = 5, rec.rate = 0.03)
+init <- init.net(i.num = 5)
+control <- control.net(type = "SIS", nsteps = 100, nsims = 1, verbose = FALSE)
 sim <- netsim(est, param, init, control)
-sim
-plot(sim, type = "formation", plots.joined = FALSE)
-plot(sim, popfrac = FALSE)
+#output
+outputdf <- sim[["stats"]][["nwstats"]][["sim1"]]
+p <- ncol(outputdf)
+#predictability
+var_obj <- mvar(data = outputdf, type = rep("g", p), lev = rep(1, p), lambdaSel = "EBIC",
+lambdaGam = .25,
+lags = 1)
+pred_obj <- predict(var_obj, outputdf, errorCon = c("RMSE", "R2"),
+errorCat = c("CC", "nCC"))
+mean(pred_obj$errors$RMSE)
